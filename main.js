f574dc2b5536e3a8f71bcacbab7ffa4eb69ba18b
7GrandDadPGN <29617380+7GrandDadPGN@users.noreply.github.com>
2024-08-07 16:39:53 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Vape V4
+// @namespace    http://7granddadpgn.github.io
+// @version      2024-07-29
+// @description  A browser script made to give enhancements on miniblox.io
+// @author       7GrandDad
+// @match        https://miniblox.io/*
+// @icon         https://miniblox.io/favicon.png
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
+// @require      https://raw.githubusercontent.com/7GrandDadPGN/VapeForMiniblox/main/injection.js
+// @run-at       document-start
+// ==/UserScript==
