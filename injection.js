e98d7861772780c8056614b406f4bfbd5c837cb1
7GrandDadPGN <29617380+7GrandDadPGN@users.noreply.github.com>
2024-08-21 19:20:57 -0600

    Randomized watermark

--- injection.js
+++ injection.js
@@ -1,6 +1,7 @@
 let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
+const vapeName = crypto.randomUUID().replaceAll("-", "").substring(16);
 
 // ANTICHEAT HOOK
 function replaceAndCopyFunction(oldFunc, newFunc) {
@@ -119,7 +120,7 @@ function modifyCode(text) {
 		});
 	`);
 
-	addReplacement('VERSION$1," | ",', '"Vape V4 v1.0.4"," | ",');
+	addReplacement('VERSION$1," | ",', `"${vapeName} v1.0.4"," | ",`);
 	addReplacement('if(!nt.canConnect){', 'nt.errorMessage = nt.errorMessage == "Could not join server. You are connected to a VPN or proxy. Please disconnect from it and refresh the page." ? "You\'re either using a detected VPN server or IP banned for cheating." : nt.errorMessage;');
 
 	// DRAWING SETUP
